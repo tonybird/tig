3920a8753f97be3e6f86a94160fae3a53eade039
chris <cburgess4397@gmail.com>
2020-03-08 19:28:20 -0600

    Update parse.clj

--- src/parse.clj
+++ src/parse.clj
@@ -8,17 +8,17 @@
   (when (not (.exists (io/file dir)))
     (println "Error: the directory specified by -r does not exist")
     (System/exit 2))
-  {:r dir :command rest})
+  {:root dir :command rest})
 
 (defn parse-d-flag [[_ dir & rest]]
   (when (or (= dir "-r") (= dir nil))
     (println "Error: the -d switch needs an argument")
     (System/exit 1))
-  {:d dir :command rest})
+  {:db dir :command rest})
 
 (defn parse-flags [args]
   (let [top-level-flags {"-r" parse-r-flag "-d" parse-d-flag}]
-    (loop [result {:command args :d ".idiot" :r "."} opts top-level-flags]
+    (loop [result {:command args :db ".idiot" :root "."} opts top-level-flags]
       (let [flag (first (:command result)) command (:command result)]
         (if (contains? opts flag)
           (recur
