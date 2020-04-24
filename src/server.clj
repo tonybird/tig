82eeb0ff04401c4ac0dac6b46e8caf3b15bdb62d
Tony Bird <tonybird@live.unc.edu>
2020-04-24 00:28:18 -0400

    Added Terrell explore tests

--- src/server.clj
+++ src/server.clj
@@ -7,27 +7,27 @@
             [hiccup.core :refer [html]]
             [hiccup.page :refer [html5]]))
 
-(defn response [status body]
+(defn- response [status body]
   {:status status  ; meaning "OK"
-   :headers {"Content-Type" "text/html"}  ; instead of e.g. "text/html"
+   :headers {"Content-Type" "text/html"}
    :body body})  ; the payload
 
 (defn- get-link [type address]
   [:a {:href (str "/" type "/" address)} address])
 
-(defn get-branch-list [dir]
+(defn- get-branch-list [dir]
   (let [branch-files (->> (str dir "/refs/heads/") io/file file-seq rest)
         branch-names (sort (map #(.getName %) branch-files))
         name->li #(vector :li (get-link "branch" %))]
     [:ul {:class "branch-list"} (map name->li branch-names)]))
 
-(defn get-head-info [dir]
+(defn- get-head-info [dir]
   (let [head-addr (str dir "/HEAD")
         head-contents (slurp head-addr)
         ref-addr (str/trim-newline (subs head-contents 16))]
     [:div {:class "head-info"} "HEAD points to ref " (get-link "branch" ref-addr)]))
 
-(defn head-response [dir]
+(defn- head-response [dir]
   (let [branch-list (get-branch-list dir)
         head-info (get-head-info dir)
         body (html5
@@ -35,25 +35,25 @@
                [:body head-info branch-list])]
     (response 200 body)))
 
-(defn branch-response [dir address]
+(defn- branch-response [dir address]
   (let [body (html5
                [:head [:title "Explore"]]
                [:body [:div [:h1 {:class "info"} "Branch"] address]])]
     (response 200 body)))
 
-(defn commit-response [dir address]
+(defn- commit-response [dir address]
   (let [body (html5
                [:head [:title "Explore"]]
                [:body [:div [:h1 {:class "info"} "Commit"] address]])]
     (response 200 body)))
 
-(defn tree-response [dir address]
+(defn- tree-response [dir address]
   (let [body (html5
                [:head [:title "Explore"]]
                [:body [:div [:h1 {:class "info"} "Tree"] address]])]
     (response 200 body)))
 
-(defn blob-response [dir address]
+(defn- blob-response [dir address]
   (let [body (html5
                [:head [:title "Explore"]]
                [:body [:div [:h1 {:class "info"} "Blob"] address]])]
@@ -62,7 +62,7 @@
 (defn- parse-uri [uri]
   (-> uri (str/split #"/") (nth 2)))
 
-(defn get-handler [dir]
+(defn- get-handler [dir]
   (fn handler [request]
     (let [uri (:uri request)
           head? (= uri "/")
@@ -74,7 +74,7 @@
         (str/starts-with? uri "/tree/") (tree-response dir address)
         (str/starts-with? uri "/blob/") (blob-response dir address)))))
 
-(defn start-server [dir]
+(defn- start-server [dir]
   (let [handler (get-handler dir)]
   (run-jetty handler {:port 3000})))
 
